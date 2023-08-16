9178ea99f0b226826f66103b92ef8960d2e2069c
chrisdotgrubb <developer.grubb@gmail.com>
2023-08-15 23:07:15 -0400

    Add cart counter and change navbar background color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavbarBS from "react-bootstrap/Navbar";
+import {useCartContext} from "../context/Context.ts";
 
 
 export default function Navbar() {
+	const cart = useCartContext();
+	
 	return (
-		<NavbarBS expand="sm" sticky={"top"}>
+		<NavbarBS expand="sm" sticky={"top"} className={"bg-dark-subtle"}>
 			<Container>
 				<NavbarBS.Brand href="/">
 					<svg
@@ -25,7 +28,21 @@ export default function Navbar() {
 						<Nav.Link href={"/manage"}>Manage</Nav.Link>
 					</Nav>
 					<Nav>
-						<Nav.Link href={"/cart"}>Cart</Nav.Link>
+						<Nav.Link href={"/cart"}>
+							{cart.cartQty ?
+								
+								<span
+									className={"fw-bold text-light me-2 border border-secondary rounded bg-secondary p-1"}>{cart.cartQty}</span>
+								
+								: false}
+							
+							<svg
+								xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+								className="bi bi-cart3" viewBox="0 0 16 16">
+								<path
+									d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+							</svg>
+						</Nav.Link>
 					</Nav>
 				</NavbarBS.Collapse>
 			</Container>
